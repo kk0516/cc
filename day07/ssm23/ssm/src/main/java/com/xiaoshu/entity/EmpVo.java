f626942d5f8b53ac60b3b30b5e87320461c2f7db
kk0516 <2369780292@qq.com>
2020-08-07 22:54:08 +0800

    提交day07

---
+++ day07/ssm23/ssm/src/main/java/com/xiaoshu/entity/EmpVo.java
@@ -0,0 +1,15 @@
+package com.xiaoshu.entity;
+
+public class EmpVo extends Emp {
+	private String cname;
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	
+}
